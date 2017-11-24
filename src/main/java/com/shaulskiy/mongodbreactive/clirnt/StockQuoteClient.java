72b3ca152769803cf73788f5383531add7dd8ba7
Mikhail Shaulskiy <mshaulskiy@gmail.com>
2017-11-24 14:22:23 +0000

    Added Spring WebFlux Client

---
+++ src/main/java/com/shaulskiy/mongodbreactive/clirnt/StockQuoteClient.java
@@ -0,0 +1,39 @@
+package com.shaulskiy.mongodbreactive.clirnt;
+
+import com.shaulskiy.mongodbreactive.domain.Quote;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+
+@Slf4j
+@Setter
+@Component
+@ConfigurationProperties("shaulskiy")
+public class StockQuoteClient {
+
+    private String host;
+    private String port;
+    private String path;
+
+
+    public Flux<Quote> getQuoteStream(){
+
+        String url = "http://" + host + port;
+
+        log.debug("Url Set to: " + url);
+
+        return WebClient.builder()
+                .baseUrl(url)
+                .build()
+                .get()
+                .uri(path)
+                .accept(MediaType.APPLICATION_STREAM_JSON)
+                .retrieve()
+                .bodyToFlux(Quote.class);
+    }
+}
